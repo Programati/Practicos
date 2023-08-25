3798d360601912b17dc55495e32368718a6b8a50
Matias Martinez <82737039+Programati@users.noreply.github.com>
2023-08-25 18:36:11 -0300

    Agregar TP1

---
+++ Practico1/Practico1/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub BGuardar_Click(sender As Object, e As EventArgs) Handles BGuardar.Click
+        TextBox3.Text = TextBox1.Text & " " & TextBox2.Text
+    End Sub
+
+    Private Sub BEliminar_Click(sender As Object, e As EventArgs) Handles BEliminar.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+    End Sub
+
+    Private Sub BSalir_Click(sender As Object, e As EventArgs) Handles BSalir.Click
+        End
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.Control And e.KeyCode = Keys.S Then
+            End
+        End If
+    End Sub
+End Class
